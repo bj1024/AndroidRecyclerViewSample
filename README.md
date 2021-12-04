d615c44906d3a125328116235962b7e0d18eb7b5
H.K <16552133+bj1024@users.noreply.github.com>
2021-12-04 20:03:13 +0900

    Add RecyclerView.ItemDecoration description.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 - Android RecyclerView Sample of horizontal and virtical,multi view type.
 
 ![Android RecyclerView Sample](assets/screen.gif "Android Recycler View Sample")
+
+
+# Tips
+
+- セル間のマージン調整は カスタムした RecyclerView.ItemDecoration クラスを addItemDecoration する。
--- app/src/main/java/com/testdomain/androidrecyclerviewsample/ui/main/MyRecyclerItemDecoration.kt
+++ app/src/main/java/com/testdomain/androidrecyclerviewsample/ui/main/MyRecyclerItemDecoration.kt
@@ -1,11 +1,9 @@
 package com.testdomain.androidrecyclerviewsample.ui.main
 
 
-import android.content.Context
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.testdomain.androidrecyclerviewsample.R
 
 
 class MyRecyclerItemDecoration(
@@ -15,29 +13,6 @@ class MyRecyclerItemDecoration(
         private val spaceRight:Int
 
         ) : RecyclerView.ItemDecoration() {
-
-//
-//    fun CustomItemDecoration(space: Int) {
-//        this.space = space
-//    }
-//
-//    fun createDefaultDecoration(context: Context): MyRecyclerItemDecoration? {
-//
-//        val spacingInPixels: Int = context.resources.getDimensionPixelSize(R.dimen.item_margin)
-//        return MyRecyclerItemDecoration(spacingInPixels)
-//    }
-//
-//    fun getItemOffsets(
-//        outRect: Rect,
-//        view: View?,
-//        parent: RecyclerView?,
-//        state: RecyclerView.State?
-//    ) {
-//        outRect.top = space
-//        outRect.left = space
-//        outRect.right = space
-//        outRect.bottom = space
-//    }
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         outRect.top = spaceTop
