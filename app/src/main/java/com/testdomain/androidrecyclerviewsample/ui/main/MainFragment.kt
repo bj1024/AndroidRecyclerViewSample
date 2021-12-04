43e61a922a03fedff38ebcc0017feb51d80a8bb2
H.K <16552133+bj1024@users.noreply.github.com>
2021-12-04 19:13:31 +0900

    Move RecyclerView init to onCreateView from onViewCreated.

--- app/src/main/java/com/testdomain/androidrecyclerviewsample/ui/main/MainFragment.kt
+++ app/src/main/java/com/testdomain/androidrecyclerviewsample/ui/main/MainFragment.kt
@@ -23,7 +23,58 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.main_fragment, container, false).apply {
+
+            val imageids = arrayOf(
+                    R.drawable.ic_baseline_apartment_24,
+                    R.drawable.ic_baseline_audiotrack_24,
+                    R.drawable.ic_baseline_cottage_24,
+                    R.drawable.ic_baseline_error_outline_24,
+                    R.drawable.ic_baseline_inbox_24,
+                    R.drawable.ic_baseline_outlet_24,
+                    R.drawable.ic_baseline_pedal_bike_24,
+                    R.drawable.ic_launcher_foreground
+            )
+
+
+            val myDataList = mutableListOf<MyData>()
+            repeat((0..99).count()) {
+                myDataList.add(
+                        MyData("No.${it+1} ",
+                                "test",
+                                imageids.random()
+                        ))
+            }
+
+
+            val recyclerViewListH = findViewById<RecyclerView>(R.id.recycler_list_h)
+            val recyclerViewAdapterListH = MyRecyclerViewAdapter(myDataList,false)
+
+            val lmh = LinearLayoutManager(context)
+            lmh.orientation = LinearLayoutManager.HORIZONTAL // 横スクロール
+
+            // RecyclerViewの初期設定
+            recyclerViewListH.apply {
+                layoutManager= lmh
+
+//            layoutManager = LinearLayoutManager(context)
+//            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                addItemDecoration(MyRecyclerItemDecoration(0,0,0,16))
+                adapter = recyclerViewAdapterListH
+            }
+
+            val lmv = LinearLayoutManager(context)
+            val recyclerViewListV = findViewById<RecyclerView>(R.id.recycler_list_v)
+            val recyclerViewAdapterListV = MyRecyclerViewAdapter(myDataList,true)
+            recyclerViewListV.apply {
+                layoutManager= lmv
+
+//            layoutManager = LinearLayoutManager(context)
+//            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                addItemDecoration(MyRecyclerItemDecoration(0,0,16,0))
+                adapter = recyclerViewAdapterListV
+            }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -37,55 +88,6 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val imageids = arrayOf(
-            R.drawable.ic_baseline_apartment_24,
-            R.drawable.ic_baseline_audiotrack_24,
-            R.drawable.ic_baseline_cottage_24,
-            R.drawable.ic_baseline_error_outline_24,
-            R.drawable.ic_baseline_inbox_24,
-            R.drawable.ic_baseline_outlet_24,
-            R.drawable.ic_baseline_pedal_bike_24,
-            R.drawable.ic_launcher_foreground
-        )
-
-
-        val myDataList = mutableListOf<MyData>()
-        repeat((0..99).count()) {
-            myDataList.add(
-                    MyData("No.${it+1} ",
-                    "test",
-                        imageids.random()
-            ))
-        }
-
-
-        val recyclerViewListH = view.findViewById<RecyclerView>(R.id.recycler_list_h)
-        val recyclerViewAdapterListH = MyRecyclerViewAdapter(myDataList,false)
-
-        val lmh = LinearLayoutManager(context)
-        lmh.orientation = LinearLayoutManager.HORIZONTAL // 横スクロール
-
-        // RecyclerViewの初期設定
-        recyclerViewListH.apply {
-             layoutManager= lmh
-
-//            layoutManager = LinearLayoutManager(context)
-//            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-            addItemDecoration(MyRecyclerItemDecoration(0,0,0,16))
-            adapter = recyclerViewAdapterListH
-        }
-
-        val lmv = LinearLayoutManager(context)
-        val recyclerViewListV = view.findViewById<RecyclerView>(R.id.recycler_list_v)
-        val recyclerViewAdapterListV = MyRecyclerViewAdapter(myDataList,true)
-        recyclerViewListV.apply {
-            layoutManager= lmv
-
-//            layoutManager = LinearLayoutManager(context)
-//            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-            addItemDecoration(MyRecyclerItemDecoration(0,0,16,0))
-            adapter = recyclerViewAdapterListV
-        }
 
     }
 
